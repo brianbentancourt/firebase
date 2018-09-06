a419fd43995d808b9ff13ee85131de523b452824
brianbentancourt <brianbentancourt9@gmail.com>
2018-09-06 17:40:08 -0300

    añadiendo login con facebook

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,46 @@
 let btnLogin = document.getElementById('btnLogin')
+let btnLoginF = document.getElementById('btnLoginF')
+let btnLogOut = document.getElementById('btnLogOut')
+let userName = document.getElementById('userName')
+
 
 firebase.auth().onAuthStateChanged(function(user){
 	if(user){
 		console.log(user)
+		userName.innerHTML = user.displayName
+		mostrarLogout()
 	}else{
 		console.log('No hay usuario')
+		userName.innerHTML = ''
+		mostrarLogin()
 	}
 })
+
+function mostrarLogin(){
+	btnLoginF.style.display = 'block'
+	btnLogin.style.display = 'block'
+	btnLogOut.style.display = 'none'
+}
+
+function mostrarLogout(){
+	btnLoginF.style.display = 'none'
+	btnLogin.style.display = 'none'
+	btnLogOut.style.display = 'block'
+}
+
+
+
+function login(userData){
+	console.log(userData)
+}
+
+function logout(){
+	console.log('sesion cerrada')
+}
+
+function error(err){
+	console.log(err)
+}
 
 btnLogin.addEventListener('click', function(){
 	event.preventDefault()
@@ -14,10 +48,29 @@ btnLogin.addEventListener('click', function(){
 	provider.addScope('https://www.googleapis.com/auth/contacts.readonly')
 	firebase.auth().languageCode = 'es';
 	firebase.auth().signInWithPopup(provider)
-	.then(function(userData){
-		console.log(userData)
-	}).catch(function(err){
-		console.log(err)
-	})
+	.then(login)
+	.catch(error)
+})
+
+btnLoginF.addEventListener('click', function(){
+	event.preventDefault()
+	const provider = new firebase.auth.FacebookAuthProvider()
+	provider.addScope('public_profile')
+
+	// para GitHub
+	//const provider = new firebase.auth.GithubAuthProvider();
+    //provider.addScope('scope');
+
+	firebase.auth().languageCode = 'es';
+	firebase.auth().signInWithPopup(provider)
+	.then(login)
+	.catch(error)
+})
+
+btnLogOut.addEventListener('click', function(){
+	event.preventDefault()
+	firebase.auth().signOut()
+	.then(logout)
+	.catch(error)
 })
 
